bc95453495ce1148bf3ff5768f199bbf6c10ce4a
yonash <janosmolnar@hu.ibm.com>
2018-10-30 20:32:34 +0100

    initialized Fast model to track users fasts

--- backend/fast/models.py
+++ backend/fast/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Fast(models.Model):
+    """ Stores fasts
+    """
+
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='fasts')
+
+    def __str__(self):
+        return self.user.username
+
+    def __unicode__(self):
+        return self.user.username
