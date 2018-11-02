fba3d8716de12bd222fe052bfc7f79a9c477d32e
yonash <janosmolnar@hu.ibm.com>
2018-11-02 16:34:32 +0100

    Modified state so we have a single state object

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,24 @@ import "./App.css";
 const API = "http://localhost:8000/api";
 
 class App extends Component {
+  state = {
+    fasts: []
+  };
+
+  fetchFasts = () => {
+    fetch(`${API}/fasts`)
+      .then(response => response.json())
+      .then(data => this.setState({ fasts: data }));
+  };
+
+  componentDidMount() {
+    this.fetchFasts();
+  }
+
   render() {
     return (
       <div className="App">
-        <FastList api={API} />
+        <FastList fasts={this.state.fasts} />
       </div>
     );
   }
--- frontend/src/components/FastList.jsx
+++ frontend/src/components/FastList.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from "react";
 
 class FastList extends Component {
-  state = {
-    fasts: []
-  };
-
-  componentDidMount() {
-    fetch(`${this.props.api}/fasts`)
-      .then(response => response.json())
-      .then(data => this.setState({ fasts: data }));
-  }
   render() {
     return (
       <React.Fragment>
         <div>FastList</div>
-        <div>{this.props.api}</div>
-        {this.state.fasts.map(fast => (
+        {this.props.fasts.map(fast => (
           <li key={fast.id}>
             {fast.user}: {fast.start} - {fast.end}
           </li>
